fc20c3930a4a3cb9144cf65679a82c51ee18fb51
Ognjen <ognjenmirkovic032@gmail.com>
2025-02-17 12:58:16 +0100

    prototype.py

---
+++ prototype/gui.py
@@ -0,0 +1,29 @@
+import pygame as pg
+import time
+import RK4
+
+def main():
+    WIDTH, HEIGHT = 600, 600
+    window = pg.display.set_mode((WIDTH, HEIGHT))
+    
+    for θ1, θ2, ω1, ω2 in RK4.calculate():
+        window.fill((255, 255, 255))
+
+        x1, y1 =  RK4.polar_to_cart(RK4.l1, θ1) 
+        x2, y2 =  RK4.polar_to_cart(RK4.l2, θ2)
+        x2 += x1
+        y2 += y1
+
+        x1 = (x1 * WIDTH//4) // 1 + WIDTH//2
+        y1 = (y1 * HEIGHT//4) // 1 + HEIGHT//2
+        x2 = (x2 * WIDTH//4) // 1 + WIDTH//2
+        y2 = (y2 * HEIGHT//4) // 1 + HEIGHT//2
+
+        pg.draw.line(window, (0, 0, 0), (WIDTH//2, HEIGHT//2), (x1, y1), 10)
+        pg.draw.line(window, (0, 0, 0), (x1, y1), (x2, y2), 10)
+        
+        pg.display.update()
+
+        time.sleep(0.1)
+
+main()
