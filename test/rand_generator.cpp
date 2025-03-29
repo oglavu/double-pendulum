c859856098b54d2c05e0d827c84864050c6eb3a4
Ognjen <ognjenmirkovic032@gmail.com>
2025-03-29 23:58:43 +0100

    rand_generator.cpp update

--- test/rand_generator.cpp
+++ test/rand_generator.cpp
@@ -1,20 +1,79 @@
 #include <iostream>
 #include <fstream>
 #include <random>
-#include <cstdlib>
+
+#define π (3.14f)
+
+#define GEN_GENERATORS(rd, range, gen, dist) \
+    std::mt19937 gen(rd()); \
+    std::uniform_real_distribution<double> dist(range.first, range.second);
+
+typedef std::pair<float, float> pairf_t;
+
+struct double4 {
+    double x, y, z, w;
+};
+
+struct args_t {
+
+    pairf_t θ1_range{0, 2*π},
+        θ2_range{0, 2*π},
+        ω1_range{-10, 10},
+        ω2_range{-10, 10};
+
+};
+
+int read_args(int argc, char* argv[], args_t& myArgs) {
+    /*
+        Reads command line arguements and parses them
+        
+        Error -1: no option's dash
+        Error -2: invalid option
+        Error -3: invalid range
+    */
+
+    float t1, t2;
+    for (int i=0; i<argc; i += 3) {
+        if (argv[i][0] != '-') {
+            return -1;
+        } 
+        if (std::string("-teta1").compare(argv[i]) == 0) {
+            t1 = myArgs.θ1_range.first  = atof(argv[i+1]);
+            t2 = myArgs.θ1_range.second = atof(argv[i+2]);
+        } else if (std::string("-teta2").compare(argv[i]) == 0) {
+            t1 = myArgs.θ2_range.first  = atof(argv[i+1]);
+            t2 = myArgs.θ2_range.second = atof(argv[i+2]);
+        } else if (std::string("-omega1").compare(argv[i]) == 0) {
+            t1 = myArgs.ω1_range.first  = atof(argv[i+1]);
+            t2 = myArgs.ω1_range.second = atof(argv[i+2]);
+        } else if (std::string("-omega2").compare(argv[i]) == 0) {
+            t1 = myArgs.ω2_range.first  = atof(argv[i+1]);
+            t2 = myArgs.ω2_range.second = atof(argv[i+2]);
+        } else {
+            return -2;
+        }
+
+        if (t1 > t2) return -3;
+
+    }
+    return 0;
+}
+
 
 int main(int argc, char *argv[]) {
     // Ensure the user provided the number of random numbers as an argument
     if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <output_file> <num_random_numbers>\n";
+        std::cerr << "Usage: " << argv[0] << " <output_file> <num_random_numbers> [<options>]\n";
         return 1;
     }
 
     // Convert input argument to an integer
-    size_t num_random = std::atoi(argv[2]);
-    if (num_random <= 0) {
-        std::cerr << "Error: Invalid number of random numbers.\n";
-        return 1;
+    uint32_t n = std::atoi(argv[2]);
+
+    args_t myArgs;
+    if (read_args(argc-3, &argv[3], myArgs) < 0) {
+        std::cerr << "Error: Bad cmd line args. Usage -teta1 {val1} {val2}, where val1 <= val2. \n";
+        return 2;
     }
 
     // Open the file in binary mode
@@ -25,25 +84,26 @@ int main(int argc, char *argv[]) {
     }
 
     // Random number generator
-    std::random_device rd;  // Seed
-    std::mt19937 gen(rd()); // Mersenne Twister RNG
-    std::uniform_real_distribution<double> dist(0.0f, 1.0f); // [0,1] range
+    std::random_device rd[4];  // Seed for Mersenne Twister RNG
+    GEN_GENERATORS(rd[0], myArgs.θ1_range, θ1_gen, θ1_dist);
+    GEN_GENERATORS(rd[1], myArgs.θ2_range, θ2_gen, θ2_dist);
+    GEN_GENERATORS(rd[2], myArgs.ω1_range, ω1_gen, ω1_dist);
+    GEN_GENERATORS(rd[3], myArgs.ω2_range, ω2_gen, ω2_dist);
 
-    struct vec4 {
-        double θ1, θ2, ω1, ω2;
-    };
+    double4* array = new double4[n];
 
-    struct vec4 v;
-    for (size_t i = 0; i < num_random; i++) {
-        v.θ1 = dist(gen) * 6.28;
-        v.θ2 = dist(gen) * 3.14;
-        v.ω1 = dist(gen) * 10;
-        v.ω2 = dist(gen) * 10;
-        file.write(reinterpret_cast<const char*>(&v), sizeof(v));
+    for (uint32_t i = 0; i < n; i++) { //dist(gen)
+        array[i].x = θ1_dist(θ1_gen);
+        array[i].y = θ2_dist(θ2_gen);
+        array[i].z = ω1_dist(ω1_gen);
+        array[i].w = ω2_dist(ω2_gen);
     }
+
+    file.write((char*)array, n * sizeof(double4));
 
     // Close the file
     file.close();
-    std::cout << "Generated " << num_random << " random numbers in '" << argv[1] <<"'.\n";
+    delete[] array;
+    std::cout << "Generated " << n << " random numbers in '" << argv[1] <<"'.\n";
     return 0;
 }
